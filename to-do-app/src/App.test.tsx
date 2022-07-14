e71bd233850daa63b16fde283c62eb7f142b0082
ThanuH <108867504+ThanuH@users.noreply.github.com>
2022-07-14 16:30:31 +0530

    Dev Phase 6

--- to-do-app/src/App.test.tsx
+++ to-do-app/src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './Main';
+import App from './Components/Main';
 
 test('renders learn react link', () => {
   render(<App />);
--- to-do-app/src/Components/Main.tsx
+++ to-do-app/src/Components/Main.tsx
@@ -3,47 +3,22 @@ import React from "react";
 //import logo from './logo.svg';
 
 import "./Style/App.css";
-
 import "./Style/Input.css";
-
-import InputField from "./Components/Home";
-
+import Home from "../Components/Pages/Home";
 // import image from './Images/back.jpg'
-
 // import { url } from 'inspector';
 
-import {
-
-  BrowserRouter as Router,
-
-  Switch,
-
-  Route,
-
-  Redirect,
-
-} from "react-router-dom";
-
-
+import {BrowserRouter as Router,Switch,Route,Redirect,} from "react-router-dom";
 
 const App: React.FC = () => {
-
   return (
-
     <Router>
-
       <Switch>
-
         <Route exact path="/" component={InputField} />
-
+        <Route exact path="/" component={InputField} />
       </Switch>
-
     </Router>
-
   );
-
 };
-
-
 
 export default App;
