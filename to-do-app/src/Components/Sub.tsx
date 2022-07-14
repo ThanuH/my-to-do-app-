9162d00045c9b6d8d995f4e4f17899d83c7d5447
ThanuH <108867504+ThanuH@users.noreply.github.com>
2022-07-14 14:47:32 +0530

    Dev Phase 4

---
+++ to-do-app/src/Components/Sub.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Style/App.css'
+const Sub =() =>{
+    return <form className='form'>
+        <div>
+        <h1 className='Heading'>To-Do-List</h1> 
+        <p className='Text'>Lets Organize Your Day-To-Day Tasks</p>
+        </div>
+    </form>
+};
+
+export default Sub;
--- to-do-app/src/Main.tsx
+++ to-do-app/src/Main.tsx
@@ -2,18 +2,24 @@ import React from 'react';
 //import logo from './logo.svg';
 import './Style/App.css';
 import './Style/Input.css'
-import InputField from './Components/InputField'
+import Home from './Components/Home'
+import Sub from './Components/Sub'
 // import image from './Images/back.jpg'
 // import { url } from 'inspector';
+import { BrowserRouter as Router, Switch, Route, Redirect,} from "react-router-dom";
 
 const App: React.FC =() =>{
   return(
-  
-    <div className="App" >
+    <Router>
       
-      <InputField/>
-      
-    </div>
+          <Switch>
+           <Route exact path="/" component={Home} />     
+           <Route path= "/sub" component={Sub}/>
+          </Switch>
+    
+        </Router>
+    
+    
   )
 }
 
