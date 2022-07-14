a3a6f85195bc9fae8cc19d65bdf6f8c6ce681f41
ThanuH <108867504+ThanuH@users.noreply.github.com>
2022-07-14 22:02:23 +0530

    Dev Phase 7

--- to-do-app/src/Components/Pages/Home.tsx
+++ to-do-app/src/Components/Pages/Home.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { useHistory } from "react-router-dom";
 import '../../Style/App.css';
+import { Input } from 'antd';
+import { Button } from 'antd';
 
 
+const { TextArea } = Input;
+
+
+const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    console.log('Change:', e.target.value);
+  };
+  
 const Home =() =>{
     const history = useHistory();
     const nextPage =()=>{history.push("/view-list")}
@@ -11,13 +20,13 @@ const Home =() =>{
     <div className='App'>
     <form >   
         <h1 className='Heading'>To-Do-List</h1> 
-        <p className='Text'>Lets Organize Your Day-To-Day Tasks</p>
+        <h3 className='Text'>Lets Organize Your Day-To-Day Tasks</h3>
         <p className='InputTxt'>Enter the title of the task</p>
-        <input type = 'input' placeholder='Enter Title' className='TitleBox' />
+        <Input placeholder='Enter Title' className='TitleBox' /><br/>
         <p className='InputTxt'>Enter the description of the task</p>
-        <textarea placeholder='Enter task description' className='DesBox'/>
-        <button type='button'  className='Button1'>Add to task list</button>
-        <button type='button'  className='Button2' onClick={nextPage}>View task list</button>
+        <TextArea showCount maxLength={100} style={{ height: 190 }} onChange={onChange} placeholder='Enter task description' className='DesBox'/>
+        <Button type="primary"  className='Button1'>Add to task list</Button>
+        <Button type="primary"  className='Button2' onClick={nextPage}>View task list</Button>
     </form>
     </div>
     )
